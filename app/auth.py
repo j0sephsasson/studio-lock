03dfd57ca80274d97d2e118615f436326d58465e
Joe Sasson <sassonjoe66@gmail.com>
2022-12-03 17:46:26 -0500

    finished building login route

--- app/auth.py
+++ app/auth.py
@@ -18,10 +18,17 @@ def login_post():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    print(username)
-    print(password)
+    user = User.query.filter_by(username=username).first()
 
-    return render_template('login.html')
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page w/ flash
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user)
+    return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
 def signup():
