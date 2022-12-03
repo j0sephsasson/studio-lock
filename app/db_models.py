7c9aea71a50dbb89b5533a4cd17df00949430c49
Joe Sasson <sassonjoe66@gmail.com>
2022-12-03 15:33:33 -0500

    added username field in db

--- app/db_models.py
+++ app/db_models.py
@@ -4,5 +4,7 @@ from flask_login import UserMixin
 ## user table ##
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = db.Column(db.Text, unique=False, nullable=False)
+    username = db.Column(db.Text, unique=True, nullable=False)
     email = db.Column(db.Text, unique=True, nullable=False)
     password = db.Column(db.Text, unique=False, nullable=False)
