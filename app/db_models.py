fd89cc7743090a42933ed1f492930aa73012e0bd
Joe Sasson <sassonjoe66@gmail.com>
2022-12-05 18:25:39 -0500

    added studios/booking tables

--- app/db_models.py
+++ app/db_models.py
@@ -8,3 +8,19 @@ class User(UserMixin, db.Model):
     username = db.Column(db.Text, unique=True, nullable=False)
     email = db.Column(db.Text, unique=True, nullable=False)
     password = db.Column(db.Text, unique=False, nullable=False)
+
+## studios table ##
+class Studio(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = db.Column(db.Text, unique=True, nullable=False)
+
+## bookings table ##
+class Bookings(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    studio_name = db.Column(db.Text, unique=False, nullable=False) ## studio name
+    username = db.Column(db.Text, unique=False, nullable=False) ## username
+    date = db.Column(db.Text, unique=False, nullable=False) ## date
+    slot_one = db.Column(db.Boolean, default=False) ## slot is booked --> default False
+    slot_two = db.Column(db.Boolean, default=False) ## slot is booked --> default False
+    slot_three = db.Column(db.Boolean, default=False) ## slot is booked --> default False
+    slot_four = db.Column(db.Boolean, default=False) ## slot is booked --> default False
