5840ba5bbf7aaedf1eac6ce03ba4a17a9069b762
Joe Sasson <sassonjoe66@gmail.com>
2022-12-21 15:43:55 -0500

    added dynamic studio_details endpoints

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import os
 from flask import Blueprint, render_template, request, flash, session, jsonify
 from flask.helpers import url_for
 from . import db
-from .db_models import User
+from .db_models import User, Studio
 from dotenv import load_dotenv
 from flask_login import login_required, current_user
 from werkzeug.utils import redirect, secure_filename
@@ -37,60 +37,62 @@ def booking():
     return render_template('booking.html', name=name)
 
 # checkout-session page of our web-app
-@main.route('/booking_post', methods=['POST'])
+@main.route('/booking_post/<studio_name>', methods=['POST'])
 @login_required
 def booking_post():
     stripe.api_key = os.getenv('STRIPE_TEST_KEY')
     STUDIO_PRICE_ID = os.getenv('STUDIO_NAME_PRICE_ID')
 
-    try:
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': STUDIO_PRICE_ID,
-                    'quantity': 3,
-                },
-            ],
-            metadata={'studio_name': 'hello sir'},
-            mode='payment',
-            success_url='http://127.0.0.1:5000/success',
-            cancel_url='http://127.0.0.1:5000/cancel',
-            automatic_tax={'enabled': True},
-        )
-    except Exception as e:
-        return str(e)
 
-    return redirect(checkout_session.url, code=303)
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[
+            {
+                # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                'price': STUDIO_PRICE_ID,
+                'quantity': 3,
+            },
+        ],
+        success_url='http://127.0.0.1:5000/success',
+        cancel_url='http://127.0.0.1:5000/cancel',
+        automatic_tax={'enabled': True},
+        mode='payment',
+        metadata={
+        "customer_id": "123456",
+        "order_id": "987654"
+        }
+    )
+    return redirect(session.url, code=303)
 
 # checkout-session webhook api of our web-app
 @main.route('/webhook', methods=['POST'])
 def webhook():
-    payload = request.data
-    sig_header = request.headers['STRIPE_SIGNATURE']
-    endpoint_secret = os.getenv('STRIPE_WEBHOOK_TESTING_KEY')
+    payload = request.json
+    session = payload['data']['object']
 
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, endpoint_secret
-        )
-    except ValueError as e:
-        # Invalid payload
-        raise e
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        raise e
+    custom_data = session['metadata']
+    print(custom_data)
+    return None
 
-    # Handle the event
-    if event['type'] == 'payment_intent.succeeded':
-      payment_intent = event['data']['object']
-      print('payment success!')
-      print(payment_intent.get('metadata'))
-    else:
-      print('Unhandled event type {}'.format(event['type']))
 
-    return jsonify(success=True)
-    
+# studio_details "handler" page of our web-app
+@main.route('/studio_details', methods=['GET'])
+def studio_details():
+    # Get the item ID from the request
+    item_id = request.args.get('studio_name')
+
+    # Redirect to the item URL with the item ID
+    return redirect(url_for('main.show_item', studio_name=item_id))
+
+# studio_details page of our web-app
+@main.route('/studio_details/<studio_name>', methods=['GET'])
+def show_item(studio_name):
+    # Retrieve the item with the specified ID
+    studio = Studio.query.filter_by(name=studio_name).first()
+    if studio:
+        return render_template('studio_details.html', studio_name=studio.name)
+
+
 # checkout-cancel page of our web-app
 @main.route('/cancel', methods=['GET', 'POST'])
 def cancel():
@@ -100,8 +102,3 @@ def cancel():
 @main.route('/success', methods=['GET', 'POST'])
 def success():
     return render_template('success.html')
-
-# studio_details page of our web-app
-@main.route('/studio_details', methods=['GET', 'POST'])
-def studio_details():
-    return render_template('studio_details.html', studio_name='Testing')
